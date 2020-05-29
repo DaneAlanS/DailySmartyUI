99cd05448c680b62e14900bd4939955bc7bb5afc
DaneAlanS <daneasimoneau@gmail.com>
2020-05-29 08:54:35 -0500

    setup redux form in searchbar.js

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
-export default class SearchBar extends Component {
-  render() {
+import { withRouter } from "react-router-dom";
+
+class SearchBar extends Component {
+  handleFormSubmit = function ({ query }) {
+    this.props.onSubmit(query);
+  };
+
+  renderInput(field) {
     return (
-      <form>
-        <input placeHolder="search daily smarty"></input>
+      <input
+        type="text"
+        placeholder="&#xf002; Search DailySmarty"
+        {...field.input}
+      />
+    );
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form
+        className={`search-bar search-bar__${this.props.page}`}
+        onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}
+      >
+        <div className="search-bar__wrapper">
+          <Field name="query" component={this.renderInput} />
+          <p>Press return to search</p>
+        </div>
       </form>
     );
   }
 }
+
+SearchBar = reduxForm({
+  form: "searchBar",
+})(SearchBar);
+
+SearchBar = withRouter(SearchBar);
+
+export default SearchBar;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
+import { reducer as form } from "redux-form";
+
+const rootReducer = combineReducers({ form });
 
 export default rootReducer;
