1f5b0b561732b76e6be19287fe473b432f047410
DaneAlanS <daneasimoneau@gmail.com>
2020-05-28 22:54:27 -0500

    setup logo component and styles

---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class extends Component {
+  render() {
+    return (
+      <div className="logo-main">
+        <img alt="daily smarty logo" src="/assets/ds_circle_logo.png"></img>
+      </div>
+    );
+  }
+}
