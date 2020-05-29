edc7bf3fa1bf3848c3d047e5ea49266e582b631a
DaneAlanS <daneasimoneau@gmail.com>
2020-05-29 08:29:53 -0500

    created recent posts and searchbar components

---
+++ src/components/searchBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <input placeHolder="search daily smarty"></input>
+      </form>
+    );
+  }
+}
